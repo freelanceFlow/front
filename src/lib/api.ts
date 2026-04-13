c3d80db50db88de6cff39c31a7190581b5d2740d
thomas <thomaspiet@outlook.fr>
2026-04-13 17:00:52 +0200

    edit api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+const api = axios.create({                                                                                                                                                                                                               
+  baseURL: '/api/',                                                                                                                                                                                                                      
+  headers: {                                                                                                                                                                                                                             
+    'Content-Type': 'application/json',                                                                                                                                                                                                  
+  },                                                                                                                                                                                                                                     
+}); 
 
 // Intercepteur pour injecter le token JWT
 api.interceptors.request.use(
